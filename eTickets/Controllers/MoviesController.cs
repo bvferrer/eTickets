5220e8226dd5bfa1091eddb012fe6845cbab37e7
Bjorn James V. FERRER <bvferrer@epdsi.ph>
2022-08-06 00:13:04 +0800

    adding controllers

---
+++ eTickets/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using eTickets.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allMovies = await _context.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
